5f35163e60c401ca5dd5eb06a6e3c1f1176684c1
tzikos <papantzikos12@gmail.com>
2023-01-31 16:11:24 +0200

    gia na doume

--- youtube_summary.py
+++ youtube_summary.py
@@ -16,30 +16,31 @@ if st.button('Submit'):
         video_id = video_url.split("watch?v=")[-1]
         try:
             transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+            transcripts_en = [t.fetch() for t in transcript_list if t.language_code == 'en']
+            if not transcripts_en:
+                st.write("No English transcripts found")
+                
+
+            text = " ".join(t['text'] for t in transcripts_en[0])
+            #prompt = f'{question} from this text: {(lambda x: " ".join(random.sample(x.split(),3500)) if len(x.split())>4097 else x)(text)}'
+            prompt = f'Answer this :{question} from this text: {text}'
+            if len(text.split())>4097:
+                st.write('The text is too long.')
+                
+            response = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=prompt,
+                temperature=0.9,
+                max_tokens=300,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0.6,
+                stop = 
+            )
+            summary = response["choices"][0]["text"]
+            st.write(summary)
         except _errors.TranscriptsDisabled:
             st.write('Subtitles are disabled for this video.')
-        transcripts_en = [t.fetch() for t in transcript_list if t.language_code == 'en']
-        if not transcripts_en:
-            st.write("No English transcripts found")
-            
-
-        text = " ".join(t['text'] for t in transcripts_en[0])
-        #prompt = f'{question} from this text: {(lambda x: " ".join(random.sample(x.split(),3500)) if len(x.split())>4097 else x)(text)}'
-        prompt = f'Answer this :{question} from this text: {text}'
-        if len(text.split())>4097:
-            st.write('The text is too long.')
-            
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=prompt,
-            temperature=0.9,
-            max_tokens=300,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0.6,
-        )
-        summary = response["choices"][0]["text"]
-        st.write(summary)
     else :
         'Provide a valid YouTube link and your question.'
 
