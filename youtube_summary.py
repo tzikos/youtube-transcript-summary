db33d50190f5a8903e4b2816d0c8cd7e1a444132
tzikos <papantzikos12@gmail.com>
2023-01-31 23:53:46 +0200

    checking for valid key

--- youtube_summary.py
+++ youtube_summary.py
@@ -13,41 +13,42 @@ openai.api_key = str(key)
 video_url = st.text_input("Enter the YouTube video URL: ")
 
 question = st.text_input('What information do you need from this video?: ')
+try:
+    if st.button('Submit'):
+        if (video_url and question and key):
+            if "watch?v=" in video_url:
+                video_id = video_url.split("watch?v=")[-1]
+            else:
+                video_id = video_url.split("/")[-1]
 
-if st.button('Submit'):
-    if (video_url and question and key):
-        if "watch?v=" in video_url:
-            video_id = video_url.split("watch?v=")[-1]
-        else:
-            video_id = video_url.split("/")[-1]
+            try:
+                transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+                transcripts_en = [t.fetch() for t in transcript_list if t.language_code == 'en']
+                if not transcripts_en:
+                    st.write("No English transcripts found")
+                    
 
-        try:
-            transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
-            transcripts_en = [t.fetch() for t in transcript_list if t.language_code == 'en']
-            if not transcripts_en:
-                st.write("No English transcripts found")
-                
-
-            text = " ".join(t['text'] for t in transcripts_en[0])
-            #prompt = f'{question} from this text: {(lambda x: " ".join(random.sample(x.split(),3500)) if len(x.split())>4097 else x)(text)}'
-            prompt = f'Answer this :{question} from this text: {text}'
-            if len(text.split())>4097:
-                st.write('The text is too long.')
-                
-            response = openai.Completion.create(
-                model="text-davinci-003",
-                prompt=prompt,
-                temperature=0.9,
-                max_tokens=300,
-                top_p=1,
-                frequency_penalty=0,
-                presence_penalty=0.6,
-                stop = 'I hope it helps'
-            )
-            summary = response["choices"][0]["text"]+'\n\nI hope it helps !'
-            st.write(summary)
-        except _errors.TranscriptsDisabled:
-            st.write('Subtitles are disabled for this video.')
-    else :
-        'Provide a valid YouTube link and your question.'
-
+                text = " ".join(t['text'] for t in transcripts_en[0])
+                #prompt = f'{question} from this text: {(lambda x: " ".join(random.sample(x.split(),3500)) if len(x.split())>4097 else x)(text)}'
+                prompt = f'Answer this :{question} from this text: {text}, with "~" to be the last character of the response'
+                if len(text.split())>4097:
+                    st.write('The text is too long.')
+                    
+                response = openai.Completion.create(
+                    model="text-davinci-003",
+                    prompt=prompt,
+                    temperature=0.9,
+                    max_tokens=1000,
+                    top_p=1,
+                    frequency_penalty=0,
+                    presence_penalty=0.6,
+                    stop = '~'
+                )
+                summary = response["choices"][0]["text"]
+                st.write(summary)
+            except _errors.TranscriptsDisabled:
+                st.write('Subtitles are disabled for this video.')
+        else :
+            'Provide a valid YouTube link and your question.'
+except openai.error.AuthenticationError:
+    print('Please provide a valid API key.')
